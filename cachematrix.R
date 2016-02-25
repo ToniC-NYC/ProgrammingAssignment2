378b40499f3f2bd5095c8e86c9efdf6f20d0542d
ToniC-NYC <anton_celic@yahoo.com>
2016-02-24 20:44:21 -0500

    save final commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  makeCacheMatrix returns a list of functions constructed 
+##  by passing an invertible matrix
 ## https://github.com/ToniC-NYC/ProgrammingAssignment2.git
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+
+
+makeCacheMatrix <- function(x = matrix())  {
+## Local instance holding a cached inverse matrix
+  m <- NULL
+  setMatrix <- function(y) {
+## <<- Operator Functions allowing access to the function instance variables
+    x <<- y
+## Remove Cached value if new matrix applied
+    m <<- NULL
+  }
+  getMatrix <- function() x
+  setInverseMatrix <- function(InverseMatrix) m <<- InverseMatrix
+  getInverseMatrix <- function() m
+## Return a list of variables pointing to above function  
+  list(setMatrix = setMatrix, 
+       getMatrix = getMatrix,
+       setInverseMatrix = setInverseMatrix,
+       getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the cached inverse matrix by passing
+## in an instance of the makeCacheMatrix function, otherwise
+## generates a new inverse matrix 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Obtain cached value from function  
+  m <- x$getInverseMatrix()
+## If cached value exist, retrieve inverse matrix and exit function
+  if(!is.null(m)) {
+    message("getting cached inverse")
+    return(m)
+  }
+## Else compute new inverse matrix
+  data <- x$getMatrix()
+  m <- solve(data,...)
+  x$setInverseMatrix(m)
+  m
 }
+
